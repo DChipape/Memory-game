9332ca1c604c17eaabfb0616cdda1baa907ea62b
DimaG@7084 <Dimakatsosharon94@gmail.com>
2024-06-20 06:16:19 -0700

    task

---
+++ script.js
@@ -0,0 +1,54 @@
+const gameBoard = document.getElementById('game-board');
+const cardValues = ['A', 'A', 'B', 'B', 'C', 'C', 'D', 'D', 'E', 'E', 'F', 'F', 'G', 'G', 'H', 'H'];
+let flippedCards = [];
+let matchedCards = [];
+
+function shuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+}
+
+shuffle(cardValues);
+
+cardValues.forEach(value => {
+    const card = document.createElement('div');
+    card.classList.add('card');
+    card.dataset.value = value;
+    card.innerHTML = `<div class="front"></div><div class="back">${value}</div>`;
+    card.addEventListener('click', flipCard);
+    gameBoard.appendChild(card);
+});
+
+function flipCard() {
+    if (flippedCards.length < 2 && !this.classList.contains('flipped')) {
+        this.classList.add('flipped');
+        flippedCards.push(this);
+
+        if (flippedCards.length === 2) {
+            checkMatch();
+        }
+    }
+}
+
+function checkMatch() {
+    const [card1, card2] = flippedCards;
+    if (card1.dataset.value === card2.dataset.value) {
+        card1.classList.add('matched');
+        card2.classList.add('matched');
+        matchedCards.push(card1, card2);
+        flippedCards = [];
+
+        if (matchedCards.length === cardValues.length) {
+            setTimeout(() => alert('You won!'), 500);
+        }
+    } else {
+        setTimeout(() => {
+            card1.classList.remove('flipped');
+            card2.classList.remove('flipped');
+            flippedCards = [];
+        }, 1000);
+    }
+}
+
